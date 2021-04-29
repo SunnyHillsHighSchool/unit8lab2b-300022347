32da9487aea999289c01f7f9ce1d8a8282d56c77
Kyle Kim <>
2021-04-29 02:15:57 +0000

    I completed Unit 8 Lab 2b.,

--- ProfNames.java
+++ ProfNames.java
@@ -2,8 +2,26 @@ public class ProfNames
 {
   public int countNames(String[][]nameArray, String lastName)
   {
-
-
-    return 0;
+    String currentName = "";
+    int total = 0;
+    for(int row=0; row<nameArray.length;row++)
+    {
+      for(int column=0; column<nameArray[0].length;column++)
+      {
+        String surname = "";
+        currentName = nameArray[row][column];
+        for(int let=0; let<currentName.length(); let++)
+        {
+        surname +=currentName.charAt(let);
+          if(surname.equals(lastName))
+          {
+            total++;
+          }
+          else
+          {}
+        }
+      }
+    }
+    return total; 
   }
 }
